0cb3d6eba538c9f6a3afc50be30d0db9f44dd417
Chintan Patoliya <91779370+Chintan3333@users.noreply.github.com>
2025-05-03 13:01:15 +0530

    Update server.js file

--- JApp/server/server.js
+++ JApp/server/server.js
@@ -13,10 +13,10 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
+// app.use(express.static(path.join(__dirname, '../client/build')));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+//   });
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
